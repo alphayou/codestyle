a370eaa367b8ea10aa9a7cdf78a0b0bf9faee230
Amano Shizuku <ifshizuku@gmail.com>
2024-06-29 18:01:00 +0800

    chore: add github workflow to auto release and publish

--- packages/eslint-config/src/types/configs.ts
+++ packages/eslint-config/src/types/configs.ts
@@ -2,13 +2,18 @@ import type { Linter } from 'eslint'
 import type { Options, PresetName } from '@/types'
 import type { AllRules, ESRules, StyleRules } from './rules'
 
+// all builder under src/configs fit this pattern
 export type ConfigBuilder = (options?: Object, preset?: PresetName) => Promise<TypedConfigItem[]>
 
+// the main options key name is almost the same as all configs name, just cut other options
 export type ConfigNames = Exclude<keyof Options, 'preset'>
 
 export interface ConfigPayload {
+  // for typegen
   typeName: string,
+  // used across project to access config
   builder: ConfigBuilder
+  // for typegen
   internal?: boolean
 }
 
